8ce1f1f4b5b5fa27f25e44a4847b2aaa56184644
Gloria Zhu <gloriazhu00@gmail.com>
2025-03-01 20:49:11 -0500

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
+<script>
+  import projects from "$lib/projects.json";
+  import Project from "$lib/Project.svelte";
+</script>
+
 <svelte:head>
   <title>home</title>
 </svelte:head>
@@ -5,3 +10,10 @@
 <h1>Gloria</h1>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor porro quasi quia consequuntur fugiat ab beatae iusto, mollitia aspernatur, rerum hic consequatur fuga, laudantium incidunt unde quae iste laboriosam recusandae!</p>
 <img src="images/spinalcord.jpg" alt="spinal cord"/>
+
+<h2>projects</h2>
+<div class="projects">
+{#each projects.slice(0, 3) as p}
+<Project data={p} hLevel="3" />
+{/each}
+</div>
