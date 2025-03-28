647abdac60343a3ed99c428a591fa0d232b29e9c
Gloria Zhu <gloriazhu00@gmail.com>
2025-03-27 22:55:38 -0400

    .

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -13,7 +13,7 @@
     let commits = [];
     let file_arr = [];
     onMount(async () => {
-        data = await d3.csv("../loc.csv", (row) => ({
+        data = await d3.csv("./loc.csv", (row) => ({
             ...row,
             line: Number(row.line), // or just +row.line
             depth: Number(row.depth),
